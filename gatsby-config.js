29fec56cd1461b36b5885ceed402fdc160fc3ab8
Alex Figueroa Chumbimune <alexfigueroa@fb.com>
2020-02-10 10:08:59 -0800

    Revert "Updates"
    This reverts commit d8fe7c6e5b394f6836c9147a0ce74949d397de15.

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: "gatsby-theme-blog",
+      options: {},
+    },
+  ],
+  siteMetadata: {
+    title: "Alex Figueroa",
+    author: "Alex Figueroa",
+    description: "",
+    siteUrl: "https://alexanderfigueroa.com",
+    social: [
+      {
+        name: "twitter",
+        url: "https://twitter.com/externconst",
+      },
+      {
+        name: "github",
+        url: "https://github.com/ajfigueroa",
+      },
+    ],
+  },
+}
